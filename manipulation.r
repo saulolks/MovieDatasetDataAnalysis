a71ed12a61f0b36ea6db2bf9bb86af5770867215
Saulo Ferreira <saulo.ferreira@grupoavantia.com.br>
2020-07-14 17:40:09 -0300

    Tratar as colunas em formato JSON do dataset

---
+++ manipulation.r
@@ -0,0 +1,84 @@
+library(rjson)
+movies_root = read.csv("Data/movies_metadata.csv", stringsAsFactors = F)
+
+# Tratamento da coluna "genre" -------------------------------------------------
+movies = vector()
+genres = vector()
+
+for(row in 1:nrow(movies_root)){
+  movie_id = movies_root$id[row]
+  data = fromJSON(movies_root$genres[row])
+  
+  movies <- c(movies, rep(movie_id, length(data)))
+  for(item in data){
+    genres <- c(genres, item$name)
+  }
+}
+
+movie_genre <- data.frame("movie_id"=movies, "genre"=genres)
+
+write.csv(
+  movie_genre,
+  file="movie_genre.csv",
+  row.names = F
+)
+
+# Tratamento de linhas com erro ------------------------------------------------
+indexes_to_remove = which(movies_root$adult != "False" & movies_root$adult != "True")
+movies_root <- movies_root[-indexes_to_remove,]
+
+# Tratamento da coluna "belongs_to_collection" ---------------------------------
+
+for(row in 1:nrow(movies_root)){
+  data = movies_root$belongs_to_collection[row]
+  if(data != ""){
+    collection = fromJSON(data)
+    movies_root$belongs_to_collection[row] <- collection$name
+  }else{
+    movies_root$belongs_to_collection[row] <- NA
+  }
+}
+
+write.csv(
+  movies_root,
+  file="Generated-Data/movies_metadata.csv",
+  row.names = F
+)
+
+# Tratamento da coluna "production_companies" ----------------------------------
+movies = vector()
+production_ids = vector()
+production_names = vector()
+
+for(row in 1:nrow(movies_root)){
+  movie_id = movies_root$id[row]
+  if(movies_root$production_companies[row] != ""){
+    data = fromJSON(movies_root$production_companies[row])
+    
+    movies <- c(movies, rep(movie_id, length(data)))
+    for(item in data){
+      production_ids <- c(production_ids, item$id)
+      production_names <- c(production_names, item$name)
+    }
+    print(length(movies))
+    print(length(production_names))
+  }
+}
+
+production_metadata <- data.frame("id"=production_ids, "name"=production_names, stringsAsFactors = F)
+indexes <- which(duplicated(production_metadata))
+production_metadata <- production_metadata[-indexes,]
+
+movies_productions <- data.frame("movie_id"=movies, "production_id"=production_ids, stringsAsFactors = F)
+
+write.csv(
+  production_metadata,
+  file="Generated-Data/production_metadata.csv",
+  row.names = F
+)
+
+write.csv(
+  movies_productions,
+  file="Generated-Data/movies_productions.csv",
+  row.names = F
+)
